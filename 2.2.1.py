62a9321cc11dc50d133debbad7200fd9bf1e13c9
qian wu <1169497909@qq.com>
2018-06-22 18:26:13 +0800

    2.2.1

--- 2.2.1.py
+++ 2.2.1.py
@@ -11,9 +11,14 @@ from __future__ import print_function
 1.编写两个 Python 函数来寻找一个列表中的最小值。数量级是 O(n)
 '''
 
+def findMin1(nums):
+    return min(nums)
+
 def findMin2(nums):
     max = 0
     for num in nums:
         if num > max:
             max = num
     return max
+
+
